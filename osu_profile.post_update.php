1896df1c94d31a0f1a8d1138176deec0dc425ea4
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-03-14 16:46:07 -0700

    Add metatag defaults. Update display of title field. Create update hook for title field and metatag defaults.

--- osu_profile.post_update.php
+++ osu_profile.post_update.php
@@ -3,6 +3,35 @@
 use Drupal\Core\Config\FileStorage;
 use Drupal\field\Entity\FieldConfig;
 
+function osu_profile_post_update_set_name_h1_formatter(&$sandbox) {
+  // Set title to be H1 and text-break.
+  $profile_layout = \Drupal::configFactory()
+    ->getEditable('core.entity_view_display.node.osu_profile.default');
+  $third_party_settings = $profile_layout->get('third_party_settings');
+  $components = $third_party_settings['layout_builder']['sections'][0]['components'];
+  if (isset($components['3a4fe79a-e52e-4fa2-99ee-3f5fec409e60'])) {
+    $components['3a4fe79a-e52e-4fa2-99ee-3f5fec409e60']['additional']['component_attributes']['block_content_attributes']['class'] = "";
+    $components['3a4fe79a-e52e-4fa2-99ee-3f5fec409e60']['configuration']['formatter'] = [
+      "type" => "text_field_formatter",
+      "label" => "hidden",
+      "settings" => [
+        "link_to_entity" => 0,
+        "wrap_tag" => "h1",
+        "wrap_class" => "text-break",
+        "wrap_attributes" => "",
+        "override_link_label" => "",
+        "token" => "",
+      ],
+      "third_party_settings" => [],
+    ];
+    $third_party_settings['layout_builder']['sections'][0]['components'] = $components;
+    $profile_layout->set('third_party_settings', $third_party_settings);
+    $profile_layout->save();
+    return t('Updated Profile name to display as H1');
+  }
+  return t('Title block uuid does not match expected.');
+}
+
 /**
  * Update osu profile to create a new field type.
  */
